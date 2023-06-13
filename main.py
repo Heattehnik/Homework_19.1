4f3a612b95017dbec9525810d855ba00f2dd73c6
Heattehnik <thegadjka@gmail.com>
2023-06-13 22:22:57 +0500

    Solved

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib.parse import urlparse, parse_qs
 
 hostName = "localhost"
 serverPort = 8080
@@ -8,12 +8,25 @@ serverPort = 8080
 class MyServer(BaseHTTPRequestHandler):
 
     def do_GET(self):
-        with open('./bootstrap/catalog.html') as file:
+        with open('./bootstrap/contacts.html') as file:
             html_content = file.read()
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
         self.wfile.write(bytes(html_content, "cp1251"))
+
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length).decode('utf-8')
+        data = parse_qs(post_data)
+        name = data['name'][0]
+        email = data['email'][0]
+        message = data['message'][0]
+        print('Name:', name)
+        print('Email:', email)
+        print('Message:', message)
+        self.send_response(200)
+        self.end_headers()
 
 
 if __name__ == "__main__":
